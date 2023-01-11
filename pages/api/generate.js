026c73875e43fc57677e1772208296495c2b864a
Bramandia Gilanglaksana Adam <gilangbram@gmail.com>
2023-01-11 18:40:58 +0700

    update 1

---
+++ pages/api/generate.js
@@ -0,0 +1,41 @@
+const bufferToBase64 = (buffer) => {
+  const base64 = buffer.toString('base64');
+  return `data:image/png;base64,${base64}`;
+};
+
+const generateAction = async (req, res) => {
+  console.log('Received request');
+
+  // Inputting the body of the request
+  const input = JSON.parse(req.body).input;
+
+  // Add fetch request to Huggingface
+  const response = await fetch(
+    `https://api-inference.huggingface.co/models/frankwright14045/sd-1-5-bram`,
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.HF_AUTH_KEY}`,
+        'Content-Type': 'application/json',
+      },
+      method: 'POST',
+      body: JSON.stringify({
+        inputs: input,
+      }),
+    }
+  );
+
+  // Check for different statuses to send proper payload
+  if (response.ok) {
+    const buffer = await response.buffer();
+    const base64 = bufferToBase64(buffer);
+    res.status(200).json({ image: base64 });
+  } else if (response.status === 503) {
+    const json = await response.json();
+    res.status(503).json(json);
+  } else {
+    const json = await response.json();
+    res.status(response.status).json({ error: response.statusText });
+  }
+};
+
+export default generateAction;
